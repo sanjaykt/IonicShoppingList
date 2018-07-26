e9d1c721ea210a73f3c67c46b5d005989a024d7a
Sanjay Tailor <st.sanjaytailor@gmail.com>
2018-07-26 10:45:55 +0530

    file removed

--- src/app/firebase.credentials.ts
+++
@@ -1,9 +0,0 @@
-
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDoUZvRTRFaH7NbyKWAKUUsHxhAOEWfj4c",
-  authDomain: "shopping-list-5bba6.firebaseapp.com",
-  databaseURL: "https://shopping-list-5bba6.firebaseio.com",
-  projectId: "shopping-list-5bba6",
-  storageBucket: "shopping-list-5bba6.appspot.com",
-  messagingSenderId: "508991982763"
-};
